50ccaa01f2ef8b82c9c446514b683b3c4d24b24b
Usama Nazir <usamanazir13@gmail.com>
2024-07-20 22:13:40 +0500

    adjusted dashboard

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -5,7 +5,7 @@ import { logout } from '@/app/actions/auth';
 
 const DashboardPage = () => {
   return (
-    <div>
+    <div className='flex justify-center items-center mx-auto flex-col gap-4'>
         DashboardPage
         <Button className='text-black max-w-40 hover:bg-gray-200' variant="bordered" onClick={async () => { await logout();}}>Logout</Button>
     </div>
